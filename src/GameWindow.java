eead9cc37044ffad4ad7313609dac6972f99291c
daotuanbk <32331250+daotuanbk@users.noreply.github.com>
2017-10-11 19:37:41 +0700

    Added GameCanvas

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GameWindow extends JFrame {
+
+    GameCanvas canvas;
 
     public GameWindow () {
     this.setSize (800, 600);
     this.setVisible(true);
+    this.setBackground(Color.BLACK);
+
+    this.canvas = new GameCanvas();
+    this.setContentPane(this.canvas);
     }
 
 }
