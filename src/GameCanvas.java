e364c00e5ef1a58ff26e01e06918372100db7dd4
daotuanbk <32331250+daotuanbk@users.noreply.github.com>
2017-10-11 20:47:41 +0700

    Added background n player

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -1,7 +1,31 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class GameCanvas extends JPanel {
+
+    BufferedImage background, player;
+
     public GameCanvas () {
+        //1. Load background
+        try {
+            background = ImageIO.read(new File("assets/images/background/0.png"));
+            player = ImageIO.read((new File("assets/images/players/straight/0.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
+    @Override
+    //2. Draw background
+    protected void paintComponent(Graphics g) {
+        g.drawImage(background, 0, 0, null);
+        g.drawImage(player, 192,510,null);
+        System.out.println("paint");
+    }
+
 }
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,17 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class GameWindow extends JFrame {
 
     GameCanvas canvas;
 
-    public GameWindow () {
-    this.setSize (800, 600);
-    this.setVisible(true);
-    this.setBackground(Color.black);
+    public GameWindow() {
+        this.setSize(800, 600);
 
-    this.canvas = new GameCanvas();
-    this.setContentPane(this.canvas);
+        this.setBackground(Color.black);
+
+        this.canvas = new GameCanvas();
+        this.setContentPane(this.canvas);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setResizable(false);
+
+
+        this.canvas.setVisible(true);
+        this.setVisible(true);
+
     }
 
 }
